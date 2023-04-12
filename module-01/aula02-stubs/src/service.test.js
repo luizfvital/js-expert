3474bde7a4779fd07cca181b6b667cef5da72831
Luiz Fernando Vital <luizfvital@gmail.com>
2023-04-12 17:33:14 +0100

    Add aula02 stubs

---
+++ module-01/aula02-stubs/src/service.test.js
@@ -0,0 +1,22 @@
+const Service = require('./service')
+const assert = require('assert')
+
+const BASE_URL_1 = 'https://swapi.dev/api/planets/1/'
+const BASE_URL_2 = 'https://swapi.dev/api/planets/2/'
+
+;(async () => {
+  {
+
+    /**
+     * This approach makes a real request
+     * 
+     * const service = new Service()
+     * const data = await service.makeRequest(BASE_URL_1)
+     */
+
+
+    const service = new Service()
+    
+  }
+})()
+
