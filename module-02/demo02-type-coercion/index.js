e51d45171bc1f3635ad0993c1e40b9a9197295f1
Luiz Fernando Vital <luizfvital@gmail.com>
2023-04-21 19:45:19 +0100

    Add modulo02 demo02-type-coercion

---
+++ module-02/demo02-type-coercion/index.js
@@ -0,0 +1,19 @@
+console.log('true + 2: ', true + 2)
+console.log('true - 2: ', true - 2)
+console.log('"21" + true: ', '21' + true)
+console.log('"21" - true: ', '21' - true)
+console.log('99999999999999999: ', 99999999999999999)
+console.log('0.1 + 0.2: ', 0.1 + 0.2)
+console.log('0.1 + 0.2 === 0.3: ', 0.1 + 0.2 === 0.3)
+console.log('3 > 2: ', 3 > 2)
+console.log('2 > 1: ', 2 > 1)
+console.log('3 > 2 > 1: ', 3 > 2 > 1)
+console.log('"21"(string) - -1: ', '21' - -1)
+console.log('"B" + "a" + +"a" +"a": ', "B" + "a" + +"a" +"a")
+console.log('"1" == 1: ', '1' == 1)
+console.log('"1" === 1: ', '1' === 1)
+
+//----------------------------------------------------------------
+
+console.log('String(123): ', String(123))
+console.log('123 + "": ', 123 + '')
